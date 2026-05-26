a067e450c9fbb929e5487208d7596c963a81c36f
Shubham <shubhammore976525@gmail.com>
2026-05-26 22:17:55 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,27 +6,33 @@ from sklearn.linear_model import LinearRegression
 # Load dataset
 df = pd.read_csv("housing.csv")
 
-# Features and target
+# Features
 X = df[['area', 'bedrooms', 'bathrooms']]
+
+# Target
 y = df['price']
 
 # Train model
 model = LinearRegression()
 model.fit(X, y)
 
-# UI
+# Title
 st.title("🏠 House Price Prediction")
 
+st.write("Enter House Details")
+
+# Inputs
 area = st.number_input("Area", min_value=100)
 
 bedrooms = st.number_input("Bedrooms", min_value=1)
 
 bathrooms = st.number_input("Bathrooms", min_value=1)
 
+# Predict Button
 if st.button("Predict Price"):
 
     features = np.array([[area, bedrooms, bathrooms]])
 
     prediction = model.predict(features)
 
-    st.success(f"Predicted House Price: ₹ {prediction[0]:,.2f}")
+    st.success(f"Predicted Price: ₹ {prediction[0]:,.2f}")
